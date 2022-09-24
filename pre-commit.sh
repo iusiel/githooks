50e710180dfcf403d52ea8f1025b3f0e3165d2a8
Iusiel <iusiel@outlook.com>
2022-09-24 20:44:15 +0800

    Added prettier to pre-commit

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
 php linters/php_lint.php
 status=$?
 ## if php_lint.php returned non zero code, exit immediately and abort git commit ## 
 [ $status -eq 0 ] && echo '' || exit 1
 node linters/js_css_lint.js
+status=$?
+[ $status -eq 0 ] && echo '' || exit 1
+npx prettier --write .
